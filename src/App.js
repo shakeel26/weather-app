9960e2ceff93c5fc0f12fabebf3672748ca2ea91
Shakeel Ahmed <shakeel.ahmed@sti2.at>
2020-11-27 13:21:56 +0100

    Add sample project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './components/Hello';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
